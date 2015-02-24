e281fa9ee3a5f0400a447f6ae8c37ca153399dd6
toye4eva01 <toye4eva01@yahoo.com>
2015-02-24 14:34:16 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+var service-box = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
